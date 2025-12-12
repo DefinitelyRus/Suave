c95553e072e795cbbdb3bbdffcec020f883d1706
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2025-12-12 12:40:45 +0800

    Create AnimatedEntity.cs

---
+++ Scripts/Entities/AnimatedEntity.cs
@@ -0,0 +1,10 @@
+﻿using Suave.Scripts.Objects;
+namespace Suave.Scripts.Entities;
+
+internal abstract class AnimatedEntity(string name, string entityId, Animation animation) : Entity(name, entityId) {
+
+	/// <summary>
+	/// The current animation being played by the entity.
+	/// </summary>
+	public Animation CurrentAnimation { get; protected set; } = animation;
+}
