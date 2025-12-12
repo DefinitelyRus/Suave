2b26ff7d2e49d4252f76b151b4f4fb36d82aadeb
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2025-12-13 03:45:59 +0800

    Create EnemyBoss.cs

---
+++ Scripts/Implementations/EnemyBoss.cs
@@ -0,0 +1,26 @@
+﻿using System.Numerics;
+using Suave.Scripts.Entities;
+using Suave.Scripts.Managers;
+
+namespace Suave.Scripts.Implementations;
+
+internal class EnemyBoss : Enemy {
+	public EnemyBoss(Vector2 position) : base(
+		name: "Boss",
+		entityId: "EnemyBoss",
+		position: position,
+		hitRadius: 32f,
+		maxHealth: 150,
+		damage: 2,
+		attackRange: 256f,
+		attackCooldown: 0.8f,
+		moveSpeed: 96f,
+		aggroRange: 392f,
+		avoidRange: 48f
+		) {
+		Name = "Boss";
+		EntityId = "EnemyBoss";
+		Position = position;
+		CurrentTexture = ResourceManager.GetTexture("Enemy Boss");
+	}
+}
