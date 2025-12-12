802c949fbabe9787c81ebf7a455eab020a181b87
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2025-12-12 12:40:21 +0800

    Create PhysicalEntity.cs

---
+++ Scripts/Entities/PhysicalEntity.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+
+namespace Suave.Scripts.Entities;
+
+internal abstract class PhysicalEntity(string name, string entityId, float hitRadius = 16f) : Entity(name, entityId) {
+	public float HitRadius { get; protected set; } = hitRadius;
+
+	public bool CollidesAt(Vector2 point) {
+		float distance = Vector2.Distance(Position, point);
+		return distance <= HitRadius;
+	}
+
+	public bool CollidesWith(PhysicalEntity other) {
+		float combinedRadius = HitRadius + other.HitRadius;
+		float distance = Vector2.Distance(Position, other.Position);
+		return distance <= combinedRadius;
+	}
+}
