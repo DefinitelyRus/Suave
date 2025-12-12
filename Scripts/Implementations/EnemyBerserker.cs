1e6f484d5f4aec5bb8d59db6f1d15d11c1194bcd
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2025-12-13 03:45:54 +0800

    Create EnemyBerserker.cs

---
+++ Scripts/Implementations/EnemyBerserker.cs
@@ -0,0 +1,26 @@
+﻿using System.Numerics;
+using Suave.Scripts.Entities;
+using Suave.Scripts.Managers;
+
+namespace Suave.Scripts.Implementations;
+
+internal class EnemyBerserker : Enemy {
+	public EnemyBerserker(Vector2 position) : base(
+		name: "Berserker",
+		entityId: "EnemyBerserker",
+		position: position,
+		hitRadius: 32f,
+		maxHealth: 20,
+		damage: 1,
+		attackRange: 72f,
+		attackCooldown: 1f,
+		moveSpeed: 24f,
+		aggroRange: 108f,
+		avoidRange: 48f
+		) {
+		Name = "Berserker";
+		EntityId = "EnemyBerserker";
+		Position = position;
+		CurrentTexture = ResourceManager.GetTexture("Enemy Berserker");
+	}
+}
