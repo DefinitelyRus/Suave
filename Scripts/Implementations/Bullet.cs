29fccc93589747624e636510640aa13be47d9661
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2025-12-13 02:35:26 +0800

    Create Bullet.cs

---
+++ Scripts/Implementations/Bullet.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+using Suave.Scripts.Entities;
+using Suave.Scripts.Managers;
+
+namespace Suave.Scripts.Implementations;
+
+internal class Bullet : Projectile {
+	public Bullet(Character owner) : base("Bullet", "Bullet", owner) {
+		CurrentTexture = ResourceManager.GetTexture("Bullet Regular 01");
+	}
+
+	public override void Launch(Vector2 direction) {
+		base.Launch(direction);
+
+		//TODO: AVFX here.
+	}
+}
