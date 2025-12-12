c79ae7a2a978a9d102b99839bd03205b0c801f98
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2025-12-13 03:45:56 +0800

    Create EnemySniper.cs

---
+++ Scripts/Implementations/EnemySniper.cs
@@ -0,0 +1,26 @@
+﻿using System.Numerics;
+using Suave.Scripts.Entities;
+using Suave.Scripts.Managers;
+
+namespace Suave.Scripts.Implementations;
+
+internal class EnemySniper : Enemy {
+	public EnemySniper(Vector2 position) : base(
+		name: "Sniper",
+		entityId: "EnemySniper",
+		position: position,
+		hitRadius: 32f,
+		maxHealth: 12,
+		damage: 3,
+		attackRange: 300f,
+		attackCooldown: 3f,
+		moveSpeed: 24f,
+		aggroRange: 450f,
+		avoidRange: 64f
+		) {
+		Name = "Sniper";
+		EntityId = "EnemySniper";
+		Position = position;
+		CurrentTexture = ResourceManager.GetTexture("Enemy Sniper");
+	}
+}
