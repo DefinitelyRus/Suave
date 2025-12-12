3998fd88f2ba606e6babc5f8202a9fa00346ba95
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2025-12-13 01:18:15 +0800

    Create Level.cs

---
+++ Scripts/Levels/Level.cs
@@ -0,0 +1,15 @@
+﻿using Suave.Scripts.Objects;
+namespace Suave.Scripts.Levels;
+
+internal class Level {
+
+	#region Enemies
+
+	public float TimeLimit { get; protected set; } = 120.0f;
+	public int Waves { get; protected set; } = 3;
+	public PackedEnemy[] Enemies { get; protected set; } = [];
+
+	#endregion
+
+	public const string MusicName = "Level 0";
+}
