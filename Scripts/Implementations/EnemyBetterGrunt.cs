e0cb8a7ba6fefbbb5b6fad0866a56f312b4b410c
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2025-12-13 03:45:51 +0800

    Create EnemyBetterGrunt.cs

---
+++ Scripts/Implementations/EnemyBetterGrunt.cs
@@ -0,0 +1,25 @@
+﻿using System.Numerics;
+using Suave.Scripts.Entities;
+using Suave.Scripts.Managers;
+namespace Suave.Scripts.Implementations;
+
+internal class EnemyBetterGrunt : Enemy {
+	public EnemyBetterGrunt(Vector2 position) : base(
+		name: "Senior Grunt",
+		entityId: "EnemyGrunt",
+		position: position,
+		hitRadius: 32f,
+		maxHealth: 24,
+		damage: 2,
+		attackRange: 128f,
+		attackCooldown: 1f,
+		moveSpeed: 64f,
+		aggroRange: 192f,
+		avoidRange: 48f
+		) {
+		Name = "Senior Grunt";
+		EntityId = "EnemyBetterGrunt";
+		Position = position;
+		CurrentTexture = ResourceManager.GetTexture("Enemy Better Grunt");
+	}
+}
