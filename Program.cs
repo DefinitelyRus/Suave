2ce683ff6e16c9097f3c83819aca80261c6c1288
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2025-12-13 03:06:12 +0800

    Start adding stuff

--- Program.cs
+++ Program.cs
@@ -1,7 +1,22 @@
-﻿namespace Suave;
+﻿using Raylib_cs;
+using Suave.Scripts.Managers;
+
+namespace Suave;
 
 internal class Program {
     static void Main() {
+        Raylib.SetTraceLogLevel(TraceLogLevel.Warning);
+        GameRenderer.Init();
+        //GameManager.Init();
+        
+        while (!Raylib.WindowShouldClose()) {
+            Raylib.BeginDrawing();
 
-    }
+            float delta = Raylib.GetFrameTime();
+
+			EntityManager.UpdateAllEntities(delta);
+
+			Raylib.EndDrawing();
+		}
+	}
 }
