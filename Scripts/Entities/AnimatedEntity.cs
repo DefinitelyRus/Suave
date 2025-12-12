19574e3d6d689fc117c7557eb8ab4e6b5228e541
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2025-12-12 15:03:45 +0800

    Reordered parameters

--- Scripts/Entities/AnimatedEntity.cs
+++ Scripts/Entities/AnimatedEntity.cs
@@ -2,7 +2,7 @@
 using Suave.Scripts.Objects;
 namespace Suave.Scripts.Entities;
 
-internal abstract class AnimatedEntity(string name, string entityId, Animation animation, Vector2 position) : Entity(name, entityId, position) {
+internal abstract class AnimatedEntity(string name, string entityId, Vector2 position, Animation animation) : Entity(name, entityId, position) {
 
 	/// <summary>
 	/// The current animation being played by the entity.
