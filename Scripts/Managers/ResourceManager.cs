effed1c3d6eb2024fe17fec9dbde3bf3b0c80ae0
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2025-12-12 14:05:34 +0800

    Create ResourceManager.cs

---
+++ Scripts/Managers/ResourceManager.cs
@@ -0,0 +1,56 @@
+﻿using Raylib_cs;
+
+namespace Suave.Scripts.Managers;
+
+internal class ResourceManager {
+
+	#region General
+
+	public const string RootPath = "Assets";
+
+	public static void PreloadAssets() {
+		string[] texturePaths = Directory.GetFiles(TexturePath);
+		foreach (string path in texturePaths) {
+			string fileName = Path.GetFileNameWithoutExtension(path);
+			Texture2D texture = Raylib.LoadTexture(path);
+
+			Textures.Add(fileName, texture);
+		}
+
+		string[] soundPaths = Directory.GetFiles(SoundPath);
+		foreach (string path in soundPaths) {
+			string fileName = Path.GetFileNameWithoutExtension(path);
+			Sound sound = Raylib.LoadSound(path);
+			Sounds.Add(fileName, sound);
+		}
+	}
+
+	#endregion
+
+	#region Textures
+
+	public const string TexturePath = RootPath + "/Textures";
+
+	public static Dictionary<string, Texture2D> Textures = [];
+
+	public static Texture2D GetTexture(string name) {
+		if (Textures.TryGetValue(name, out Texture2D texture)) return texture;
+		return default;
+	}
+
+	#endregion
+
+	#region Sounds
+
+	public const string SoundPath = RootPath + "/Sounds";
+
+	public static Dictionary<string, Sound> Sounds = [];
+
+	public static Sound GetSound(string name) {
+		if (Sounds.TryGetValue(name, out Sound sound)) return sound;
+		return default;
+	}
+
+	#endregion
+
+}
