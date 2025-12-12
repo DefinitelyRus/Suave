043ffda61dd21422b38733de1fb424f1cd637178
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2025-12-13 03:15:17 +0800

    Create StateManager.cs

---
+++ Scripts/Managers/StateManager.cs
@@ -0,0 +1,15 @@
+﻿namespace Suave.Scripts.Managers;
+
+internal class StateManager {
+	public enum States {
+		Playing,
+		Paused,
+		Menu,
+		Win,
+		Lose
+	}
+
+	public static States CurrentState { get; set; } = States.Menu;
+
+	public static bool IsPaused => CurrentState == States.Paused;
+}
