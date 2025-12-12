cb26fc0983d608dd722b04b0b10187a02466c091
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2025-12-13 03:12:03 +0800

    Create InputManager.cs

---
+++ Scripts/Managers/InputManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Numerics;
+using Raylib_cs;
+namespace Suave.Scripts.Managers;
+
+internal class InputManager {
+	public const MouseButton Parry = MouseButton.Left;
+	public const MouseButton Dash = MouseButton.Right;
+	public const KeyboardKey Pause = KeyboardKey.Space;
+	public const KeyboardKey Reset = KeyboardKey.F5;
+
+	public static void Update() {
+
+	}
+}
