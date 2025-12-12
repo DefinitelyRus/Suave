cb00d5a5dead37d60e127e639ac8c7fbef0836df
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2025-12-12 15:15:43 +0800

    Create Utilities.cs

---
+++ Scripts/Tools/Utilities.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+
+namespace Suave.Scripts.Tools;
+
+internal static class Utilities {
+
+	public static float GetAngle(Vector2 direction) {
+		float angle = MathF.Atan2(direction.Y, direction.X);
+		return angle;
+	}
+
+	public static Vector2 GetDirection(float angle) {
+		float x = MathF.Cos(angle);
+		float y = MathF.Sin(angle);
+		return new Vector2(x, y);
+	}
+}
