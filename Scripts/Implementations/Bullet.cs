a96a54cae706c4d6cb2989cb56c2ffda8837affb
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2025-12-16 18:53:55 +0800

    Removed unused override

--- Scripts/Implementations/Bullet.cs
+++ Scripts/Implementations/Bullet.cs
@@ -1,5 +1,4 @@
-﻿using System.Numerics;
-using Suave.Scripts.Entities;
+﻿using Suave.Scripts.Entities;
 using Suave.Scripts.Managers;
 
 namespace Suave.Scripts.Implementations;
@@ -7,11 +6,5 @@ namespace Suave.Scripts.Implementations;
 internal class Bullet : Projectile {
 	public Bullet(Character owner) : base("Bullet", "Bullet", owner) {
 		CurrentTexture = ResourceManager.GetTexture("Bullet Regular 01");
-	}
-
-	public override void Launch(Vector2 direction) {
-		base.Launch(direction);
-
-		//TODO: AVFX here.
 	}
 }
