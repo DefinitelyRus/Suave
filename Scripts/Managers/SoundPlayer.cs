a3c07e181ed774759adeb616e508bc2465f8eb08
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2025-12-19 17:49:59 +0800

    Fixed memory leak related to audio

--- Scripts/Managers/SoundPlayer.cs
+++ Scripts/Managers/SoundPlayer.cs
@@ -9,12 +9,17 @@ internal class SoundPlayer {
 		Raylib.SetMasterVolume(0.15f);
 	}
 
-	public static void Play(Sound sound) {
+	public static void Play(Sound sound, float unloadAfter = 5f) {
 		Raylib.PlaySound(sound);
+
+		Task.Run(async () => {
+			await Task.Delay((int) (unloadAfter * 1000));
+			Raylib.UnloadSound(sound);
+		});
 	}
 
-	public static void Play(string name) {
+	public static void Play(string name, float unloadAfter = 5f) {
 		Sound sound = ResourceManager.GetSound(name);
-		Play(sound);
+		Play(sound, unloadAfter);
 	}
 }
